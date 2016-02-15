0594bb523cd5e124c4fa365a12c79800b00355c1
ben-dalton <benadalton@gmail.com>
2016-02-15 10:01:14 -0500

    index route and first react component set up

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,16 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hellow from Main component</h1>
+    )
+  }
+}
+
+// let documentReady = () => {
+//   ReactDOM.render(
+//     <Main />,
+//     document.getElementById('react')
+//   );
+// };
+// 
+// $(documentReady);
